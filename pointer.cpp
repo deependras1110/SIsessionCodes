e470360b8799e94e57db9666db357a13430620ce
deependras1110 <44043708+deependras1110@users.noreply.github.com>
2021-02-23 18:58:48 -0600

    Add files via upload

---
+++ pointer.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std; 
+
+class exercise{
+    string name;
+    int difficulty;
+
+public:
+    exercise(string name,int difficulty){
+        this->name=name;
+        this->class=class;
+    }
+    
+};
+int main (){
+    exercise e1("squats",1);
+    exercise e2 ("lunges",2);
+    exercise e3("burpees",3);
+
+    //create an exercise pointer and ask the user if the want to do another exercise or quite, if they want
+    //to then print the name of the exercise and the complexity
+
+}
